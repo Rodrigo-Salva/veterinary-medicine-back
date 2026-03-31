675ae2d4d09e0e80b86e4911e2c6046b6036d947
Rodrigo Salva <rodrigo.salva@tecusp.edu.pe>
2026-03-30 23:27:37 -0500

    feat: connect public routes to main API

--- app/infrastructure/adapters/api/routes.py
+++ app/infrastructure/adapters/api/routes.py
@@ -14,8 +14,10 @@ from app.infrastructure.adapters.api.routes_billing import router as billing_rou
 from app.infrastructure.adapters.api.routes_search import router as search_router
 from app.infrastructure.adapters.api.routes_role import router as role_router
 from app.infrastructure.adapters.api.notifications import router as notifications_router
+from app.infrastructure.adapters.api.routes_public import router as public_router
 
 router = APIRouter()
+router.include_router(public_router)
 router.include_router(pet_router)
 router.include_router(owner_router)
 router.include_router(appointment_router)
